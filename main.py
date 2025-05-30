628739b85e58868543c2f9bfbd03cc89a62fff74
Adria Vargas <avargas@creativeelectron.com>
2025-05-30 15:26:32 -0700

    preferences displayed

--- main.py
+++ main.py
@@ -22,18 +22,35 @@ from PySide6.QtWidgets import (
 
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QLabel
 
-#LOAD JSON DATA 
-with open ("data.json", "r", encoding="utf-8") as file:
-    data = json.load(file)
-
-print("printing data: ")
-print (data[0]["title"])
+#FUNCTION DECL
+#---------------------------------------------------------------
+#loads json data into a list
+def readJSON(file, key): 
+    with open (file, "r") as f:
+        data = json.load(f)
+    if isinstance(data, dict) and key in data:
+        return data[key]
+    else: 
+        return None
+#----------------------------------------------------------------
 
 #LOAD APPLICATION 
 app = QApplication(sys.argv)
-#window = MainWindow()
 loader = QUiLoader()
-window = loader.load("main.ui", None)
-window.show()
+selectionWindow = loader.load("main.ui", None)
+descriptors = readJSON("data.json", 'descriptors')
+prefGrid = selectionWindow.findChild(QGridLayout, "preferenceGrid")
+if prefGrid:
+    print("grid found")
+    cols = 5 
+    for i, descriptor in enumerate(descriptors):
+        row = i // cols
+        col = i % cols
+        checkbox = QCheckBox(descriptor)
+        prefGrid.addWidget(checkbox, row, col)
+else: 
+    print("error")
+selectionWindow.show()
 app.exec()
