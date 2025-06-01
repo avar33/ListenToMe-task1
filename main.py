f6af0a2f5c0aeab94c88f7d3428c605d8fa54033
Adria Vargas <avargas@creativeelectron.com>
2025-05-31 21:41:39 -0700

    attempt at resizing display widget

--- main.py
+++ main.py
@@ -8,16 +8,12 @@ from PySide6.QtWidgets import (
     QLCDNumber,
     QLineEdit,
     QMainWindow,
-    QProgressBar,
     QPushButton,
-    QRadioButton,
-    QSlider,
-    QSpinBox,
-    QTimeEdit,
     QVBoxLayout,
     QHBoxLayout,
     QGridLayout,
     QWidget,
+    QSizePolicy
 )
 
 from PySide6.QtWidgets import QApplication
@@ -40,17 +36,34 @@ def readJSON(file, key):
 app = QApplication(sys.argv)
 loader = QUiLoader()
 selectionWindow = loader.load("main.ui", None)
+
 descriptors = readJSON("data.json", 'descriptors')
+
+searchButton = selectionWindow.findChild(QPushButton, "searchButton")
+if searchButton:
+    print("button found")
+    
 prefGrid = selectionWindow.findChild(QGridLayout, "preferenceGrid")
 if prefGrid:
-    print("grid found")
+    print("prefGrid found")
     cols = 5 
     for i, descriptor in enumerate(descriptors):
         row = i // cols
         col = i % cols
         checkbox = QCheckBox(descriptor)
         prefGrid.addWidget(checkbox, row, col)
-else: 
-    print("error")
+
+displayGrid = selectionWindow.findChild(QGridLayout, "displayGrid")
+if displayGrid:
+    print("displayGrid found")
+
+displayInfoWidget = loader.load("displayInfoWidget.ui", selectionWindow)
+displayInfoWidget.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+if displayInfoWidget:
+    print("displayInfoW found")
+    displayInfoWidget.setStyleSheet("background-color: lightblue;")
+
+displayGrid.addWidget(displayInfoWidget, 0, 0)
+
 selectionWindow.show()
 app.exec()
