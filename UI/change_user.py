b3133f3fb7650c815be992e75055ba82c8215d95
Adria Vargas <avargas@creativeelectron.com>
2025-06-12 21:34:26 -0700

    small updates

--- UI/change_user.py
+++ UI/change_user.py
@@ -1,9 +1,9 @@
 from PySide6.QtWidgets import (
-    QMessageBox,
     QPlainTextEdit,
     QDialogButtonBox
 )
 from PySide6.QtCore import Signal, QObject
+from UI.ui_helpers import createErrorAlert
 
 class ChangeUser(QObject):
     username_changed = Signal(str)  # signal for username change
@@ -25,11 +25,18 @@ class ChangeUser(QObject):
 
     def handle_ok(self):
         username = self.username_text.toPlainText()
-        print("User saved:", username)
-        self.username_changed.emit(username)  # emit signal with new username
+        if self.find_invalid_folder_chars(username):
+            createErrorAlert("Username has invalid characters. Please pick a new user name.")
+        else:
+            print("User saved:", username)
+            self.username_changed.emit(username)  # emit signal with new username
 
     def handle_cancel(self):
         print("User change canceled")
 
     def show_change_user_dialog(self):
         self.window.show()
+
+    def find_invalid_folder_chars(self, name):
+        invalid_chars = set('<>:"/\\|?*')
+        return [char for char in name if char in invalid_chars]
